1c36e3c6646af8c216510bdb7873d5119517e078
ivanovich1071 <163851033+ivanovich1071@users.noreply.github.com>
2024-04-08 21:16:53 +0300

    add games done

--- main_OOP.py
+++ main_OOP.py
@@ -1,5 +1,7 @@
+import random
+
 class Hero:
-    def __init__(self, name, health=100, attack_power=20):
+    def __init__(self, name, health, attack_power):
         self.name = name
         self.health = health
         self.attack_power = attack_power
@@ -9,12 +11,39 @@ class Hero:
 
     def is_alive(self):
         return self.health > 0
-hero1 = Hero("Герой 1")
-hero2 = Hero("Герой 2")
 
-print(f"{hero1.name} атакует {hero2.name}...")
-hero1.attack(hero2)
-print(f"У {hero2.name} осталось здоровья: {hero2.health}")
+class Game:
+    def __init__(self, player, computer):
+        self.player = player
+        self.computer = computer
 
-print(f"{hero1.name} жив? {hero1.is_alive()}")
-print(f"{hero2.name} жив? {hero2.is_alive()}")
+    def start(self):
+        while self.player.is_alive() and self.computer.is_alive():
+            print(f"Ход игрока {self.player.name}...")
+            self.player.attack(self.computer)
+            print(f"У {self.computer.name} осталось здоровья: {self.computer.health}")
+
+            if not self.computer.is_alive():
+                print(f"{self.player.name} победил!")
+                break
+
+            print(f"Ход компьютера {self.computer.name}...")
+            self.computer.attack(self.player)
+            print(f"У {self.player.name} осталось здоровья: {self.player.health}")
+
+            if not self.player.is_alive():
+                print(f"{self.computer.name} победил!")
+
+player_name = input("Введите имя игрока: ")
+player_health = int(input("Введите здоровье игрока (10-250): "))
+player_attack_power = int(input("Введите силу удара игрока (10-20): "))
+
+computer_health = random.randint(10, 250)
+computer_attack_power = random.randint(10, 20)
+
+player = Hero(player_name, player_health, player_attack_power)
+computer = Hero("Компьютер", computer_health, computer_attack_power)
+
+game = Game(player, computer)
+game.start()
+
